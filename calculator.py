35c5c86c6d6676b08340d9e36f6c5adfe5bc390c
Dawid Dymke <dawid584@yahoo.com>
2022-08-21 11:08:49 +0200

    Add values

---
+++ calculator.py
@@ -0,0 +1,10 @@
+from ast import Return
+import logging
+import sys
+from unittest import result
+logging.basicConfig(level=logging.DEBUG)
+sum = 0,00
+
+action = float(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:  "))
+
+
