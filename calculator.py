2bf3bf144ced88fb1a58f0fa7985f52fc06796fa
Dawid Dymke <dawid584@yahoo.com>
2022-08-21 11:13:41 +0200

    Add checking

--- calculator.py
+++ calculator.py
@@ -8,3 +8,21 @@ sum = 0,00
 action = float(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:  "))
 
 
+def checking():
+
+    if action < 5 and  action != 0:
+          return True
+    
+if checking() == True:    
+      logging.info("Podaj składnik 1: ")
+      first_number = float(input("Podaj pierwszą cyfre "))
+      logging.info("Podaj składnik 2: ")
+      second_number = float(input("Podaj drugą cyfre "))
+else:
+    exit()
+
+
+
+
+
+
